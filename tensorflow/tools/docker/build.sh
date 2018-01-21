a0b68c666b8a06d237cc6776183ab8cd31055fcb
cfregly <chris@fregly.com>
2018-01-21 08:48:53 +0000

    updated

---
+++ tensorflow/tools/docker/build.sh
@@ -0,0 +1,2 @@
+docker build -t pipelineai/tensorflow-gpu:1.5.0 -f Dockerfile.pipeline-gpu .
+docker build -t pipelineai/tensorflow-cpu:1.5.0 -f Dockerfile.pipeline-cpu .
---
+++ tensorflow/tools/docker/push.sh
@@ -0,0 +1,2 @@
+docker push pipelineai/tensorflow-gpu:1.5.0
+docker push pipelineai/tensorflow-cpu:1.5.0
